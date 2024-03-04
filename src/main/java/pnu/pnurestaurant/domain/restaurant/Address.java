3dbd8ba2325ebf4db1f2d8d877e3b2071f80430d
ddglackrp <ddglackrp@naver.com>
2024-03-04 23:19:51 +0900

    Docs "Add Address class attribute"
    Address class 카카오 위치 Id 속성 추가

--- src/main/java/pnu/pnurestaurant/domain/restaurant/Address.java
+++ src/main/java/pnu/pnurestaurant/domain/restaurant/Address.java
@@ -9,4 +9,18 @@ public class Address {
     private String latitude; // 위도
 
     private String longitude; // 경도
+
+    private String locationId;
+
+    public Address() {
+    }
+
+    public Address(String latitude, String longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Address(String locationId) {
+        this.locationId = locationId;
+    }
 }
