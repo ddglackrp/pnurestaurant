a57331cb07ccd77ad2cf9aec72c604cd7aac84e0
ddglackrp <ddglackrp@naver.com>
2024-03-09 23:24:25 +0900

    Feat "시간 자동 저장 어노테이션 추가"

--- src/main/java/pnu/pnurestaurant/domain/TimeStamp.java
+++ src/main/java/pnu/pnurestaurant/domain/TimeStamp.java
@@ -1,17 +1,21 @@
 package pnu.pnurestaurant.domain;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class TimeStamp {
     @CreatedDate
     private LocalDateTime createdAt;
 
-    @LastModifiedBy
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
 }
