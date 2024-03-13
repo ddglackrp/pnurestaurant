e25c82eb8d377e872cce95c93efa5c437bc09368
ddglackrp <ddglackrp@naver.com>
2024-03-13 20:49:41 +0900

    Feat "동일성 비교 함수, @Builder 위치 변경"
    - Entity 동일성 비교를 위한 equals, hashCode 함수 추가
    - @Builder 위치 변경

--- src/main/java/pnu/pnurestaurant/domain/restaurant/Restaurant.java
+++ src/main/java/pnu/pnurestaurant/domain/restaurant/Restaurant.java
@@ -9,12 +9,12 @@ import pnu.pnurestaurant.domain.TimeStamp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static lombok.AccessLevel.PRIVATE;
 
 @Entity
 @Getter
-@Builder
 @AllArgsConstructor(access = PRIVATE)
 public class Restaurant extends TimeStamp {
 
@@ -30,21 +30,45 @@ public class Restaurant extends TimeStamp {
     @Embedded
     private Address address;
 
+    private Double googleRating;
+
+    private Double kakaoRating;
+
+    private Double studentRating;
+
+    private String restaurantPictureUrl;
+
     @OneToMany(mappedBy = "restaurant")
     private List<Review> reviews = new ArrayList<>();
 
+    protected Restaurant() {
+    }
 
-    private Double googleRating;
-    private Double kakaoRating;
-    private Double studentRating;
-    private String restaurantPictureUrl;
-
-
-    public Restaurant() {
+    @Builder
+    public Restaurant(String name, FoodType foodType, Address address, Double googleRating, Double kakaoRating, Double studentRating, String restaurantPictureUrl) {
+        this.name = name;
+        this.foodType = foodType;
+        this.address = address;
+        this.googleRating = googleRating;
+        this.kakaoRating = kakaoRating;
+        this.studentRating = studentRating;
+        this.restaurantPictureUrl = restaurantPictureUrl;
     }
 
     public void changeStudentRating(Double rating){
         this.studentRating = rating;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurant that = (Restaurant) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && foodType == that.foodType && Objects.equals(address, that.address) && Objects.equals(googleRating, that.googleRating) && Objects.equals(kakaoRating, that.kakaoRating) && Objects.equals(studentRating, that.studentRating) && Objects.equals(restaurantPictureUrl, that.restaurantPictureUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, foodType, address, googleRating, kakaoRating, studentRating, restaurantPictureUrl);
+    }
 }
